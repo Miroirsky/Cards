f92d4a7e5ea24385faa13f6310262e59f5f43bdd
Miroirsky <nigan.midi@gmail.com>
2026-03-29 11:12:57 +0200

    Adjust pity calculation divisor for rarity

--- rng.js
+++ rng.js
@@ -2218,7 +2218,7 @@ function rollItem() {
 
         if (getEquippedCount('Pity') > 0) {
             pity = parseFloat((pity + getEquippedCount('Pity')).toFixed(4));
-            pity = parseFloat(Math.max(0, pity - Math.pow(chosen.rarity, 0.1) / 25).toFixed(4));
+            pity = parseFloat(Math.max(0, pity - Math.pow(chosen.rarity, 0.1) / 250).toFixed(4));
         }
 
         // XP gain: sqrt(roll chance)
