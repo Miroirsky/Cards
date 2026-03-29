011c290e31fe60841242321b47c1ae249dfb7c11
Miroirsky <nigan.midi@gmail.com>
2026-03-29 11:10:49 +0200

    Adjust pity calculation formula in rng.js

--- rng.js
+++ rng.js
@@ -2218,7 +2218,7 @@ function rollItem() {
 
         if (getEquippedCount('Pity') > 0) {
             pity = parseFloat((pity + getEquippedCount('Pity')).toFixed(4));
-            pity = parseFloat(Math.max(0, pity - Math.pow(chosen.rarity, 0.2) / 20).toFixed(4));
+            pity = parseFloat(Math.max(0, pity - Math.pow(chosen.rarity, 0.1) / 25).toFixed(4));
         }
 
         // XP gain: sqrt(roll chance)
